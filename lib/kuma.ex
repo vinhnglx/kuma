30741cb60eeb0f56fb98bc8027832001d8ddc90c
Vincent <vinh.nglx@gmail.com>
2017-06-16 15:57:25 +0800

    First commit

---
+++ lib/kuma.ex
@@ -0,0 +1,18 @@
+defmodule Kuma do
+  @moduledoc """
+  Documentation for Kuma.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kuma.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kuma_test.exs
@@ -0,0 +1,8 @@
+defmodule KumaTest do
+  use ExUnit.Case
+  doctest Kuma
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
